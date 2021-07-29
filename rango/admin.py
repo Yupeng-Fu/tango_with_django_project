b0088867eb50d33336a18e4777920b5baa8840e5
“Yupeng <2500338f@student.gla.ac.uk>
2021-07-29 18:27:00 +0800

    chapter9

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 # Add in this class to customise the Admin Interface
@@ -10,3 +11,4 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
